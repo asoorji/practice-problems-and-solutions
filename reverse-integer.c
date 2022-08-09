2a66e22349017749e62c50199b31cc560746a232
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-08-09 15:01:26 +0200

    Update reverse-integer.c

--- reverse-integer.c
+++ reverse-integer.c
@@ -1,22 +1,24 @@
+//Reverse an integer 
+
 //Reverse an integer
-
 #include <stdio.h>
-int main()
-{    
 
-int x;
+int rev(int x) {
 int y = 0;
 int mod;
-   
   printf("Enter (above 1 digit number): ");
   scanf("%d", &x);
-   
-  while(x != 0){
+
+while(x != 0){
     y = y*10 + x%10;
-    // printf("Y: %d\n", y);
-   x = x/10;
+    x = x/10;
   }
-  printf("Reversed number is: %d", y);
-   
-   return 0;
+printf("Output: %d", y);
+}
+
+int main()
+{    
+//The argument can be any interger value
+rev(0);
+return 0;
 } 
