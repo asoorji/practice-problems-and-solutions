fb03747305c17532ff7158f733c3425522fba914
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-08-09 14:11:51 +0200

    Update reverse-integer.c

--- reverse-integer.c
+++ reverse-integer.c
@@ -1,1 +1,20 @@
 
+//Reverse an integer
+
+#include <stdio.h>
+int main()
+{    
+
+int x = 123;
+int y = 0;
+int mod;
+   
+  while(x != 0){
+    y = y*10 + x%10;
+    printf("Y: %d\n", y);
+   x = x/10;
+  }
+  printf("Reversed number is: %d", y);
+   
+   return 0;
+} 
