6203c7dd107b5fb2ad679c11b50d92a22ac91d17
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-10-04 17:50:41 +0200

    Create malloc.c

---
+++ malloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - introduction to malloc and free
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+    int *tab;
+
+    tab = malloc(sizeof(*tab) * 5);
+    tab[0] = 98;
+    tab[1] = -1024;
+    tab[2] = 402;
+    tab[3] = 77;
+    tab[4] = 88;
+    printf("%d, %d, %d, %d, %d\n", tab[0], tab[1], tab[2], tab[3], tab[4]);
+    return (0);
+}
