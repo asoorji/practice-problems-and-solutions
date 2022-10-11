edfefaa02fe7f1c65882bed3c0716f1777e6abd5
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-10-11 18:51:22 +0200

    Macro that acts like function

---
+++ macro3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MUL(a,b) a*b
+
+/**
+ * main - Macro that acts like function
+ */
+
+int main()
+{
+    
+    int ans = MUL(2,3);
+    
+    printf("The answer is %d", ans);
+
+    return 0;
+}
