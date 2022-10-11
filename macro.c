0cba79dfcf05139b5fa8916f71575d012afbb290
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-10-11 10:58:45 +0200

    Macro in c

--- macro.c
+++ macro.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #define A 10
 
+/**
+*main - shows simple implementation of a macro in c
+*A is the macro name
+*10 is the character sequence
+*/
+
 int main()
 {
-    printf("Hello, World!\n");
-    
+   
     int x = A;
     
     printf("Output: %d", x);
