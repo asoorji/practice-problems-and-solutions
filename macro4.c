9e9755fc7cafd601be6ee37cb4fa0951db80f715
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-10-11 19:28:05 +0200

    Update macro4.c

--- macro4.c
+++ macro4.c
@@ -4,7 +4,7 @@
                   else \
                   printf("%d is maximum", b)
 /**
- * main - calls the function
+ * main - calls the function. It give error after being called when undefined (#undef)
  * MAX: defined as a macro function for printing maximum value
  * @a: first arguments
  * @b: second arguments
@@ -13,5 +13,9 @@
 int main()
 {
     MAX(6,3);
+    
+    // #undef MAX
+    // MAX(5,4);
+    
     return 0;
 }
