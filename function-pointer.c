af3e14cd1adbdf9f84ee5151b6c44e821f6e0b34
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-10-12 12:50:09 +0200

    Create function-pointer.c

---
+++ function-pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * @ptr: Function pointer
+ * Resource: https://www.youtube.com/watch?v=qaszuaFXRTA
+ */
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int (*ptr)(int, int) = add;
+    int c = ptr(7,5);
+    
+    printf("Ouptut: %d\n", c);
+
+    // To get the address use this instead
+    //int c = ptr(9,5);
+    // printf("Ouptut: %p\n", &c);
+    
+    return 0;
+}
